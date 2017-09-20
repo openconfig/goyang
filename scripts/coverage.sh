cb6b22249b0836db9c603bb086d2a50acbc6a5c5
Rob Shakir <robjs@google.com>
2017-09-19 22:26:55 -0700

    Fix typo.

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -2,7 +2,7 @@
 
 go get github.com/go-playground/overalls && go get github.com/mattn/goveralls
 
-overalls -project=github.com/openconfig/ygot -covermode=count -debug
+overalls -project=github.com/openconfig/goyang -covermode=count -debug
 goveralls -coverprofile=overalls.coverprofile -service travis-ci
 
 
