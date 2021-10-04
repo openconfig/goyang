6b7bc2f46817218e2dc7153f45735a3159bae943
wenovus <wenbli@google.com>
2021-10-04 16:19:25 -0700

    Trigger CI

--- pkg/yang/yangtype.go
+++ pkg/yang/yangtype.go
@@ -242,7 +242,7 @@ type YangType struct {
 	Enum             *EnumType   `json:",omitempty"` // enum name to value, "status" is lost
 	Units            string      `json:",omitempty"` // units to be used for this type
 	Default          string      `json:",omitempty"` // default value, if any
-	HasDefault       bool        `json:",omitempty"` // Whether the type has a default.
+	HasDefault       bool        `json:",omitempty"` // whether the type has a default.
 	FractionDigits   int         `json:",omitempty"` // decimal64 fixed point precision
 	Length           YangRange   `json:",omitempty"` // this should be processed by section 12
 	OptionalInstance bool        `json:",omitempty"` // !require-instances which defaults to true
