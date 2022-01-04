2a523d8d7a3564d36ddd1fa7e19704d9850963e0
wenovus <wenbli@google.com>
2022-01-04 15:42:01 -0800

    Add another test case

--- pkg/yang/find_test.go
+++ pkg/yang/find_test.go
@@ -192,7 +192,7 @@ func TestFindGrouping(t *testing.T) {
 
 					revision 01-01-01 { description "the start of time"; }
 
-					container c { leaf b { type string; } }
+					container c { leaf b { type string; } uses s:g; }
 				}`,
 			"sysdb": `
 				module sysdb {
@@ -209,6 +209,44 @@ func TestFindGrouping(t *testing.T) {
 		},
 		inName:            "s:g",
 		wantGroupNodePath: "/sysdb/g",
+	}, {
+		desc: "grouping that uses another grouping both in different modules",
+		inMods: map[string]string{
+			"dev": `
+				module dev {
+					prefix d;
+					namespace "urn:d";
+					import dev2 { prefix "de2"; }
+
+					revision 01-01-01 { description "the start of time"; }
+
+					container c { leaf l { type string; } uses de2:g; }
+				}`,
+			"dev2": `
+				module dev2 {
+					prefix d2;
+					namespace "urn:d2";
+					import dev3 { prefix "de3"; }
+
+					revision 01-01-01 { description "the start of time"; }
+
+					grouping g { leaf a { type string; } uses de3:gg; }
+				}`,
+			"dev3": `
+				module dev3 {
+					prefix d3;
+					namespace "urn:d3";
+
+					revision 01-01-01 { description "the start of time"; }
+
+					grouping gg { leaf b { type string; } }
+				}`,
+		},
+		inNode: func(ms *Modules) (Node, error) {
+			return FindNode(ms.Modules["dev2"], "g")
+		},
+		inName:            "de3:gg",
+		wantGroupNodePath: "/dev3/gg",
 	}}
 
 	for _, tt := range tests {
