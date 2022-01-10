6ed5eaa9ef714cb42aed42a11ac8b37af2d45893
wenovus <wenbli@google.com>
2022-01-10 13:03:39 -0800

    add another test case

--- pkg/yang/find_test.go
+++ pkg/yang/find_test.go
@@ -247,6 +247,44 @@ func TestFindGrouping(t *testing.T) {
 		inName:            "de3:gg",
 		wantGroupNodePath: "/dev3/gg",
 	}, {
+		desc: "grouping that uses another grouping both in different modules but prefix is wrong",
+		inMods: map[string]string{
+			"dev": `
+				module dev {
+					prefix d;
+					namespace "urn:d";
+					import dev2 { prefix "de2"; }
+
+					revision 01-01-01 { description "the start of time"; }
+
+					container c { leaf l { type string; } uses de2:g; }
+				}`,
+			"dev2": `
+				module dev2 {
+					prefix d2;
+					namespace "urn:d2";
+					import dev3 { prefix "de3"; }
+
+					revision 01-01-01 { description "the start of time"; }
+
+					grouping g { leaf a { type string; } uses de3:gg; }
+				}`,
+			"dev3": `
+				module dev3 {
+					prefix dev3;
+					namespace "urn:dev3";
+
+					revision 01-01-01 { description "the start of time"; }
+
+					grouping gg { leaf b { type string; } }
+				}`,
+		},
+		inNode: func(ms *Modules) (Node, error) {
+			return FindNode(ms.Modules["dev2"], "g")
+		},
+		inName:          "d3:gg",
+		wantCannotFound: true,
+	}, {
 		desc: "grouping that uses another grouping both in different modules but uses wrong context node",
 		inMods: map[string]string{
 			"dev": `
